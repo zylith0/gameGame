ee66504caa5d8535b95df58521293accd43fd88a
s1003789 <s1003789@t302213-1940l.ths.katyisd.org>
2022-04-28 10:57:22 -0500

    hi <3

--- gameGame/src/gameGame/Board.java
+++ gameGame/src/gameGame/Board.java
@@ -4,10 +4,12 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.File;
 
 import javax.swing.Timer;
@@ -16,11 +18,11 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Board extends JPanel implements ActionListener{
+public class Board extends JPanel implements ActionListener, MouseListener{
 	
 	private int DELAY = 10;
 	private Timer timer;
-	private BufferedImage path;
+	private Image path;
 	private JPanel pathPanel,menu;
 	
 	public Board() {
@@ -29,23 +31,25 @@ public class Board extends JPanel implements ActionListener{
 	public void initBoard(){
 		setFocusable(true);
 		this.setLayout(new GridBagLayout());
+		addMouseListener(this);
 		GridBagConstraints c = new GridBagConstraints();
-		c.gridwidth=2;
+		c.gridwidth=1;
 		c.gridheight=1;
 		c.weightx=1;
 		c.weighty=1;
 		c.fill = c.BOTH;
 		GridBagConstraints c2 = new GridBagConstraints();
-		c2.gridwidth=2;
+		c2.gridwidth=1;
 		c2.gridheight=1;
 		c2.weightx=0.3;
 		c2.weighty=1;
 		c2.fill = c2.BOTH;
 		try {
-			path = ImageIO.read(new File("path.png"));
+			path = ImageIO.read(new File("path.png")).getScaledInstance(1500, 1080, Image.SCALE_SMOOTH);
 		}
 		catch(Exception e) {e.printStackTrace();}
 		pathPanel = new JPanel();
+		pathPanel.setOpaque(false);
 		pathPanel.setBackground(Color.black);
 		menu = new JPanel();
 		menu.setBackground(Color.green);
@@ -56,13 +60,38 @@ public class Board extends JPanel implements ActionListener{
 	}
 	@Override
     public void paintComponent(Graphics g) {
-		g.drawImage(path, 0,0, null);
-        super.paintComponent(g);
+		super.paintComponent(g);
+		g.drawImage(path, 0, 0, null);
         Toolkit.getDefaultToolkit().sync();
     }
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		this.repaint();
+		
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		System.out.println(e.getX() + " | " + e.getY());
+		
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 }
